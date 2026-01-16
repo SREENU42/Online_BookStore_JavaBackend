76e5f956a9c802b71ae7b465760810970c759c47
SREENU42 <sreenulam4@gmail.com>
2026-01-16 14:00:31 +0530

    First Commit-online_Book_Store

---
+++ src/main/java/com/onlinebookstore/service/BookService.java
@@ -0,0 +1,32 @@
+package com.onlinebookstore.service;
+
+import com.onlinebookstore.model.Book;
+import com.onlinebookstore.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public Book addBook(Book book) {
+        return repository.save(book);
+    }
+
+    public void deleteBook(Long id) {
+        repository.deleteById(id);
+    }
+}
